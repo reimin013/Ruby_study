be682f13f78eb9b4eae1c1a355f88051005684f3
reimi <utatatan.013@gmail.com>
2020-03-21 16:10:41 +0900

    [Add] FizzBuzzプログラム

---
+++ fizz_buzz.rb
@@ -0,0 +1,23 @@
+# elseのあとの"puts number"は"puts number.to_s"が回答例。すべて文字列に揃える。
+
+def fizz_buzz(number)
+
+ if number % 3 ==0 && number % 5 == 0
+ 	puts "FizzBuzz"
+ elsif number % 3 == 0
+ 	puts "Fizz"
+ elsif number % 5 == 0
+ 	puts "Buzz"
+ else
+ 	puts number
+ end
+
+end
+
+puts "1以上の数字を入力してください"
+
+input = gets.to_i
+
+puts "結果は、、、"
+
+puts fizz_buzz(input)
